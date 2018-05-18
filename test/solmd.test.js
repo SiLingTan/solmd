28eae9a2b149e723a3235e187e2a5f31141cd519
Dane Pilcher <danepilch@gmail.com>
2018-05-18 17:44:08 -0400

    start refactor

--- test/solmd.test.js
+++ test/solmd.test.js
@@ -1,35 +1,31 @@
 /* eslint-env node, mocha */
 const assert = require('assert');
-const minimist = require('minimist');
 const fs = require('fs');
-const Solmd = require('../src');
+const solmd = require('../src');
 const expected = require('./solmd/expected');
 
 describe('solmd', () => {
-  it('GavCoin', () => {
-    const args = minimist(['test/solmd/general/GavCoin.sol', '--dest', 'test/output.md']);
-    return Solmd.default.build(args)
+  it('GavCoin', () => (
+    solmd('test/solmd/general/GavCoin.sol', { dest: 'test/output.md' })
       .then(() => fs.readFileSync('./test/output.md', 'utf8'))
       .then((res) => {
         assert.equal(res, expected.general.GavCoin, 'did not produce the correct output');
-      });
-  });
+      })
+  ));
 
-  it('should have no ToC', () => {
-    const args = minimist(['test/solmd/notoc/GavCoin.sol', '--dest', 'test/output.md', '--notoc']);
-    return Solmd.default.build(args)
+  it('should have no ToC', () => (
+    solmd('test/solmd/notoc/GavCoin.sol', { dest: 'test/output.md', noToc: true })
       .then(() => fs.readFileSync('./test/output.md', 'utf8'))
       .then((res) => {
         assert.equal(res, expected.notoc.GavCoin, 'did not produce the correct output');
-      });
-  });
+      })
+  ));
 
-  it('should have events', () => {
-    const args = minimist(['test/solmd/events/GavCoin.sol', '--dest', 'test/output.md']);
-    return Solmd.default.build(args)
+  it('should have events', () => (
+    solmd('test/solmd/events/GavCoin.sol', { dest: 'test/output.md' })
       .then(() => fs.readFileSync('./test/output.md', 'utf8'))
       .then((res) => {
         assert.equal(res, expected.events.GavCoin, 'did not produce the correct output');
-      });
-  });
+      })
+  ));
 });
